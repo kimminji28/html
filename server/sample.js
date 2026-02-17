130779d7df96eab2113457d56f769f158974c432
kimminji28 <dkdyz78@gmail.com>
2026-02-17 19:45:24 +0900

    sample.js

--- server/sample.js
+++ server/sample.js
@@ -1,13 +1,13 @@
 const removeBoard = (e) => {
-  const tr = e.target.parentElement.parentElement;
-  const bno = tr.children[0].innerText;
+  const tr = e.target.parentElement.parentElement; // 삭제버튼 기준 tr 선택
+  const bno = tr.children[0].innerText; // 글번호
 
   fetch("http://localhost:3000/board_delete/" + bno)
     .then((resp) => resp.json())
     .then((data) => {
       console.log(data);
       if (data.retCode == "OK") {
-        //화면 row 삭제
+        // 화면에서 row 삭제.
         const tr = e.target.parentElement.parentElement;
         tr.remove();
       } else {
@@ -19,23 +19,24 @@ const removeBoard = (e) => {
     });
 };
 
-function makeRow(ele = {}) {
+function makeRow(elem = {}) {
   const tr = document.createElement("tr");
-  //보여줄 항목
-  for (let prof of ["BOARD_NO", "TITLE", "WRITER"]) {
+  // 보여줄 항목.
+  for (let prop of ["BOARD_NO", "TITLE", "WRITER"]) {
     const td = document.createElement("td");
-    td.innerHTML = ele[prof];
+    td.innerHTML = elem[prop];
     tr.appendChild(td);
   }
-  //td , button
-  const deltd = document.createElement("td");
+  // td, button 시작.
+  const td = document.createElement("td");
   const btn = document.createElement("button");
-  btn.innerHTML = `삭제`;
-  deltd.appendChild(btn);
-  tr.appendChild(deltd);
+  btn.innerHTML = "삭제";
+  td.appendChild(btn);
+  tr.appendChild(td);
 
-  //btn에 이벤트 등록
+  // btn에 이벤트 등록.
   btn.addEventListener("click", removeBoard);
 
+  // 작업 반환.
   return tr;
 }
